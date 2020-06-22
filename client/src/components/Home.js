9d88b118f190485a658fad677d44c2b31d3bc606
Max Sultan <maxsultakent@gmail.com>
2020-06-21 18:14:32 -0600

    added updated logo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import { Image } from 'semantic-ui-react'
-import STICKY from './img/STICKY.jpg'
+import froggy from './img/froggy.png'
 
 export default function Home({ bugs }) {
 
     return (
         <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+            <Image src={froggy} style={{maxHeight:'300px', maxWidth:'300px'}}></Image>
             <h1>DashBoard</h1>
-            <Image src={STICKY} style={{maxHeight:'300px', maxWidth:'300px'}}></Image>
             <SearchBar bugs={bugs}/>
         </div>
     )
